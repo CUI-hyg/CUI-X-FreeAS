ed9141d6697079b52ca395195ffdeddb00398739
CUI-hyg <33583919@qq.com>
2025-08-10 15:27:51 +0800

    Update _init_.py,修改版本号，正式发布v0.7.0-2508B56
    修改版本号
    正式发布v0.7.0

--- CUI-X-FreeAS/_init_.py
+++ CUI-X-FreeAS/_init_.py
@@ -1,8 +1,7 @@
-# CUI X-HiOS 下一代多智能体协同OS。
+# CUI X-FreeAS 下一代多智能体协同OS。
 # CUI Dev. 2025. All rights reserved.
 # This project use MIT License.
 # 这个项目使用MIT许可证。
 # Developed by CUI-Hyg.
-# CUI X-HiOS Ver 0.11.202507M2
-
-__version__ = "0.12.202507M4_PreviewAlpha_α"
+# CUI X-FREEAS Ver 0.7.0-202508B57
+__version__ = "0.7.0-202508B57"
